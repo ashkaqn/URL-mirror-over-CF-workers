5d7a042ad2156ab16595a6fabfe120890400fc2b
ashkaqn <ashkaneb01@gmail.com>
2024-05-23 11:53:11 +0330

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 addEventListener(
     "fetch", event => {
         let url = new URL(event.request.url);
-        url.hostname = "drive.solorroce.workers.dev";                        
+        url.hostname = "drive.soloroce.workers.dev";                        
         url.protocol = "https";
         let request = new Request(url, event.request);
         event.respondWith(
