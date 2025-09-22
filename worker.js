ed4b73444262dc1f0add52277b05a3c5f367e6b0
ashkaqn <ashkaneb01@gmail.com>
2025-09-22 23:22:32 +0330

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,7 +2,9 @@ addEventListener(
     "fetch", event => {
         let url = new URL(event.request.url);
         url.hostname = "example.com";                        
-        url.protocol = "https";
+        url.protocol = "https"; //http or https
+        //url.pathname = "example";
+        url.port = "443";
         let request = new Request(url, event.request);
         event.respondWith(
             fetch(request)
